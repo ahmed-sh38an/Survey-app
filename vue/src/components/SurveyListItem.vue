3f507a862208c27931cbd49fe3fbbcd6f8c89ce9
Ahmed Shaban <ahmed.sh38an@gmail.com>
2023-01-04 17:14:44 +0200

    public view

--- vue/src/components/SurveyListItem.vue
+++ vue/src/components/SurveyListItem.vue
@@ -28,7 +28,19 @@
         </svg>
         Edit
       </router-link>
-      <button
+      <div class="flex items-center">
+        <a 
+          :href="`/view/survey/${survey.slug}`"
+          target="_blank"
+          class="h-8 w-8 flex items-center justify-center rounded-full border border-transparent text-sm text-indigo-500 focus:ring-2
+          focus:ring-offset-2 focus:ring-indigo-500"
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+          </svg>
+
+        </a>
+        <button
         v-if="survey.id"
         type="button"
         @click="$emit('delete', survey)"
@@ -49,6 +61,7 @@
           />
         </svg>
       </button>
+      </div>
     </div>
   </div>
 </template>
